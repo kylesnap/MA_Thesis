a8dc6b74057d54b5bb6c75eb01faa9778429c35f
Kyle Dewsnap <kyle.dewsnap@gmail.com>
2021-09-16 14:44:51 -0700

    Reduced overhead by removing unnecessary variables and containers; added confirmation.

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@
 
 struct CellParam {
     int n;
-    int b1;
+    float b1;
 };
 
 template<typename Iterator>
@@ -23,12 +23,18 @@ void cartProduct(std::set<int> const &n, std::set<float> const &b1, Iterator out
     // output: vector of tuples.)
     for (int i : n) {
         for (float j : b1) {
-            struct CellParam temp;
+            CellParam temp;
             temp.n = i;
             temp.b1 = j;
             *out++ = temp;
         }
     }
+}
+
+template<typename T>
+void printSet(std::set<T> set) {
+    // Prints set elements
+    std::for_each(set.begin(), set.end(), [](const T &e) { std::cout << e << " "; } );
 }
 
 template<typename T>
@@ -44,22 +50,22 @@ T strToType(std::string const &str, int range) {
 }
 
 template<typename T>
-std::vector<T> parse_params(std::string str, int range) { // Here
+std::set<T> parse_params(std::string str, int range) { // Here
     // Takes in user entered parameters, cleans them, then outputs 2D array of numeric parameters.
-    std::vector<T> result;
+    std::vector<T> entry;
 
     while (!empty(str)) {
         std::size_t found = str.find(',');
-        result.push_back(strToType<T>(str.substr(0, found), range));
+        entry.push_back(strToType<T>(str.substr(0, found), range));
         str = found == std::string::npos ? "" : str.substr(++found);
     }
 
-    return result;
+    return std::set<T>(entry.begin(), entry.end());
 }
 
 int main() {
-    std::vector<int> sampleSizes;
-    std::vector<float> betaOne;
+    std::set<int> n;
+    std::set<float> b1;
     std::string entSizes, entBOne;
 
     std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
@@ -72,8 +78,8 @@ int main() {
     std::cout << "Beta 1 (First Parameter): ";
     getline (std::cin, entBOne);
     try {
-        sampleSizes = parse_params<int>(entSizes, 1);
-        betaOne = parse_params<float>(entBOne, -1);
+        n = parse_params<int>(entSizes, 1);
+        b1 = parse_params<float>(entBOne, -1);
     }
     catch (std::invalid_argument& e) {
         std::cout << "Error in parsing your arguments: Please only use numerals and commas in input line." << std::endl;
@@ -95,25 +101,24 @@ int main() {
     timeinfo = localtime(&rawtime);
 
     // Ask whether random should be seeded (for testing!)
-    char seed;
+    char entry;
     do {
         std::cout << "Would you like to seed random? [y/n]: ";
-        std::cin >> seed;
-    } while (!std::cin.fail() && seed != 'y' && seed != 'n');
-    (seed == 'y') ? srand(69) : srand(time(&rawtime));
+        std::cin >> entry;
+    } while (!std::cin.fail() && entry != 'y' && entry != 'n');
+    (entry == 'y') ? srand(69) : srand(time(&rawtime));
 
     //File IO object
-    char file;
     do {
         std::cout << "Would you like to output the results to a CSV? [y/n]: ";
-        std::cin >> file;
-    } while (!std::cin.fail() && file != 'y' && file != 'n');
+        std::cin >> entry;
+    } while (!std::cin.fail() && entry != 'y' && entry != 'n');
 
     std::streambuf *buf;
     std::ofstream f;
 
-    // If file is yes, then set the stream buffer to point to an output stream to file.
-    if (file == 'y') {
+    // If entry is yes, then set the stream buffer to point to an output stream to entry.
+    if (entry == 'y') {
         char fName [32];
         strftime(fName, 32, "simOut_%d%h%y_%H%M%S.csv", timeinfo);
         f.open(fName);
@@ -123,18 +128,24 @@ int main() {
         // Else, set the stream to just point to cout!
         buf = std::cout.rdbuf();
     }
-
     // Finally, refer the stream buffer to this output object.
     std::ostream out(buf);
-    out << "Test!" << std::endl;
 
-    // Makes table of inputs!
-    std::set<int> n = std::set<int>(sampleSizes.begin(), sampleSizes.end());
-    std::set<float> b1 = std::set<float>(betaOne.begin(), betaOne.end());
+    // Confirm parameters
+    std::cout << "~~~~~~~~~~~~~~~~~~~~ Confirm? ~~~~~~~~~~~~~~~~~~~~" << std::endl;
+    std::cout << "N: ";
+    printSet<int>(n);
+    std::cout << "\nB1: ";
+    printSet<float>(b1);
+
+    do {
+        std::cout << "\n[y/n]: ";
+        std::cin >> entry;
+    } while (!std::cin.fail() && entry != 'y' && entry != 'n');
+    if (entry == 'n') exit(EXIT_SUCCESS);
 
     std::vector<CellParam> allCells;
     cartProduct(n, b1, back_inserter(allCells));
-    // TO DO: Test cartProduct with the new struct, then add confirm prompt and make Simulation class.
 
     return 0;
 }
