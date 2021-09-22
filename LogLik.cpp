cdf00fa5bf03ea50dcdc51f4b50b3c8096e409f6
Kyle Dewsnap <kyle.dewsnap@gmail.com>
2021-09-21 17:19:14 -0700

    Tested LogLik func.

--- LogLik.cpp
+++ LogLik.cpp
@@ -22,8 +22,9 @@ double LogLik::logLik(const colvec &theta) {
     // g(theta; x) = sum(i = 0 -> k) { x[i]theta[i] } = x * theta' (where x[1, k] and theta[1, k])
 
     // l(theta) = (i = 0 -> N) { y[i]ln(f(theta;x[i])) * [1 - f(theta; x[i])]*ln(1 - f(theta; x[i]))
+
     g = _x * dlib::trans(theta); // Linear combination of x and theta
-    for (int i = 0; i <= g.nc(); i++) {
+    for (int i = 0; i < g.nr(); i++) {
         double temp = logf(g(i)); // Applying logfunc to i-th element of the combination of x and theta.
         int y = int(_y(i)); // Getting i-th y var.
         loglik += (y * log(temp)) + ((1 - y) * log(1 - temp));
--- tests/LogLik_Tests.cpp
+++ tests/LogLik_Tests.cpp
@@ -9,20 +9,47 @@
 #include "../LogLik.h"
 
 TEST_CASE("Tests for LogLik Function") {
+    dlib::matrix<double, 2, 2> x;
+    dlib::matrix<double, 2, 1> y;
+    colvec t;
+
     SECTION("#1") {
 
-        dlib::matrix<double, 2, 2> x;
-        dlib::matrix<double, 2, 1> y;
-        colvec t;
-
-        x = 1, 1,
-        0, 0;
+        x = 1,1,
+        0,0;
         y = 1,
         0;
-        t = 1,
-        1;
+        t = 1, 1;
 
         LogLik test = LogLik(x, y);
-        REQUIRE(test.logLik(t) == -0.8200752);
+        CHECK(test.logLik(t) == Catch::Detail::Approx(-0.8200752));
+    }
+
+    SECTION("#2") {
+
+        x = 1.1,0.2,
+        -6.9,4.20;
+        y = 0,
+        1;
+        t = 0.25, 7.5;
+
+        LogLik test = LogLik(x, y);
+        REQUIRE(test.logLik(t) == Catch::Detail::Approx(-1.931562));
+    }
+
+    dlib::matrix<double, 3, 2> x2;
+    dlib::matrix<double, 3, 1> y2;
+    SECTION("#3") {
+
+        x2 = 1,23,
+        1,22,
+        1,21;
+        y2 = 1,
+        0,
+        1;
+        t = 1, -1;
+
+        LogLik test = LogLik(x2, y2);
+        REQUIRE(test.logLik(t) == Catch::Detail::Approx(-42));
     }
 }
