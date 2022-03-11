513b5c09e46076a350c8e2db2d1f36ebdb94097e
Kyle Dewsnap <kyle.dewsnap@gmail.com>
2022-03-11 14:33:37 -0800

    Added chapter five!

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@
 int run_chap4(gsl_rng *r, std::ostream *out) {
     // Runs the random factorial design of chapter four.
 
-    int reps = 20;
+    int reps = 5000;
     std::tuple<float, float, float, float> trialParams = {0,1,1,0};
     std::tuple<float, float> noSat = {-1, 5};
     std::vector<float> temp;
@@ -27,10 +27,10 @@ int run_chap4(gsl_rng *r, std::ostream *out) {
 
     for (int i = 0; i < reps; i++) {
         temp.clear();
-        int n = static_cast<int>(gsl_ran_flat(r, 50, 2000));
+        int n = static_cast<int>(gsl_ran_flat(r, 250, 2500));
         std::tuple<float, float> betaM = {gsl_ran_flat(r, 0, 1), gsl_ran_flat(r, 2, 50)};
         std::tuple<float, float> betaF = {gsl_ran_flat(r, 0, 1), gsl_ran_flat(r, 2, 50)};
-        std::tuple<float, float> groupProp = {gsl_ran_flat(r, 0, 1), -1};
+        std::tuple<float, float> groupProp = {gsl_ran_flat(r, 0.1, 0.9), -1};
         std::get<1>(groupProp) = 1 - std::get<0>(groupProp);
 
         // 25% change of no satisficing in each group (~% of no satisficing in both groups)
@@ -40,6 +40,52 @@ int run_chap4(gsl_rng *r, std::ostream *out) {
         cell = new SimCell(n, 1, betaM, betaF, groupProp, trialParams, r, false);
         cell->toVec(temp, true);
         *out << cell->run();
+    }
+
+    return 0;
+}
+
+int run_chap5(gsl_rng *r, std::ostream *out) {
+    // Runs the blocked random factorial design of chapter five.
+
+    int sampleSize[3] = {250, 1000, 2500};
+    float betaModes[3] = {0, 0.1, 0.25};
+    float betaK[3] = {10, 25, 50};
+
+    // Make a list of beta distribution params
+    std::vector<std::tuple<float, float>> betaDists;
+    for (float i : betaModes) {
+        for (float j : betaK) {
+            betaDists.emplace_back(std::make_tuple(i, j));
+        }
+    }
+
+    int reps = 25;
+    std::tuple<float, float, float, float> trialParams = {0,1,0,1};
+    std::vector<float> temp;
+    SimCell *cell;
+
+    for (int n : sampleSize) {
+        for (auto betaM : betaDists) {
+            for (auto betaF : betaDists) {
+                for (int i = 0; i < reps; i++) {
+                    // Get two numbers, each bigger than 0.1, smaller than 0.9.
+                    float propMale = 1, propFemale = 1;
+                    while (propMale + propFemale >= 1) {
+                        propMale = (float) gsl_ran_flat(r, 0.1, 0.9);
+                        propFemale = (float) gsl_ran_flat(r, 0.1, 0.9);
+                    }
+
+                    // Run simulation.
+                    temp.clear();
+                    std::tuple<float, float> groupProp = {propMale, propFemale};
+                    cell = new SimCell(n, 1, betaM, betaF, groupProp, trialParams, r, false);
+                    cell->toVec(temp, true);
+                    *out << cell->run();
+                }
+            }
+        }
+
     }
 
     return 0;
@@ -97,9 +143,11 @@ int main() {
 
     *out << "I,N,NM,NF,NX,BM_A,BM_B,BF_A,BF_B,"
             "BTRUE_0,BTRUE_1,BTRUE_F,BTRUE_X,ERR_VAR,"
-            "BHAT_0,BHAT_1,BHAT_F,BSE_0,BSE_1,BSE_F,RSQ,NS,OBS_F" << std::endl;
+            "BHAT_0,BHAT_1,BHAT_F,BSE_0,BSE_1,BSE_F,RSQ,"
+            "NBHAT_0,NBHAT_1,NBHAT_F,NBSE_0,NBSE_1,NBSE_F,NRSQ,NS,OBS_F" << std::endl;
 
     if (entry == '4') run_chap4(r, out);
+    if (entry == '5') run_chap5(r, out);
     // CHAP 5 LINE HERE.
 
     if (out != &std::cout) delete out;
